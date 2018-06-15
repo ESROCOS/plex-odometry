df76f999155f8ba3594f44fe88590b2494512f6f
Benjamin <benjamin.kisliuk@dfki.de>
2018-06-15 10:29:07 +0000

    added dummy infrastructure

--- dummy_mc_producer/dummy_mc_producer.cc
+++ dummy_mc_producer/dummy_mc_producer.cc
@@ -1,15 +1,24 @@
 /* User code: This file will not be overwritten by TASTE. */
 
 #include "dummy_mc_producer.h"
+#include <base_support/baseTypes.h>
+#include <base_support/Base-commands-Motion2DConvert.hpp>
 
 void dummy_mc_producer_startup()
 {
     /* Write your initialization code here,
        but do not make any call to a required interface. */
+	
 }
 
 void dummy_mc_producer_PI_clock()
 {
-    /* Write your code here! */
+    base::commands::Motion2D base_mc(0,0);
+
+    asn1SccBase_commands_Motion2D asn_mc;
+    
+    asn1SccBase_commands_Motion2D_toAsn1(asn_mc, base_mc);
+    
+    kb_teleop_RI_consume_mc(&asn_mc);
 }
 
--- dummy_pose_consumer/dummy_pose_consumer.cc
+++ dummy_pose_consumer/dummy_pose_consumer.cc
@@ -1,6 +1,8 @@
 /* User code: This file will not be overwritten by TASTE. */
 
 #include "dummy_pose_consumer.h"
+#include <base_support/baseTypes.h>
+#include <base_support/Base-Pose2DConvert.hpp>
 
 void dummy_pose_consumer_startup()
 {
@@ -10,6 +12,10 @@ void dummy_pose_consumer_startup()
 
 void dummy_pose_consumer_PI_pose_in(const asn1SccBase_Pose2D *IN_pose)
 {
-    /* Write your code here! */
+  base::Pose2D base_pose; 
+  	
+  asn1SccBase_Pose2D_fromAsn1(base_pose, *IN_pose);
+
+  std::cout << "got pose: " << base_pose << std::endl;
 }
 
